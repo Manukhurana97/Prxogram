f1a9481f84878c66f058a122ee9fa3e6740d30f3
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2020-03-15 14:57:37 +0530

    Update FollowPixoServiceApplication.java

--- BackEnd/FollowPixoService-1/src/main/java/com/example/demo/FollowPixoServiceApplication.java
+++ BackEnd/FollowPixoService-1/src/main/java/com/example/demo/FollowPixoServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -9,6 +10,13 @@ public class FollowPixoServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FollowPixoServiceApplication.class, args);
+	}
+	
+	@LoadBalanced
+	@Bean
+	RestTemplate restTemplate()
+	{
+		return new RestTemplate();
 	}
 
 }
